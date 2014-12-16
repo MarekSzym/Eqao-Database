24b3e7a329ec18dadd4a00dd566a8fb7eb639a86
MarekSzym <marek.szymczyk.841@my.aquinas.me>
2014-12-16 10:29:43 -0500

    window.onload added to main.js file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+  // Great success! All the File APIs are supported.
+} else {
+  alert('The File APIs are not fully supported in this browser.');
+}
 
+window.onload = function() {
+		var fileInput = document.getElementById('fileInput');
+		var fileDisplayArea = document.getElementById('fileDisplayArea');
+                var fileDisplayMessageArea = document.getElementById('fileDisplayMessageArea');
+                
+		fileInput.addEventListener('change', function(e) {
+			var file = fileInput.files[0];
+			var textType = /text.*/;
 
+			if (file.type.match(textType)) {
+				var reader = new FileReader();
+
+				reader.onload = function(e) {
+                                        fileDisplayMessageArea.innerText = 'Your file looks like this';
+                                        
+                                        fileDisplayArea.innerText = reader.result;
+                                        data = reader.result;
+                                        var mySudoku = new Sudoku(data);
+                                        
+                                        
+				};
+
+				reader.readAsText(file);	
+                               
+			} else {
+				fileDisplayArea.innerText = "File not supported!";
+			}
+		});
+};
